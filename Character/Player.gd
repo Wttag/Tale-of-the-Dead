ecd92bccc3331f199c957fb5f47819a294cf519c
Wttag <fancsa100@gmail.com>
2023-03-19 14:50:31 +0100

    Revert "Revert "Revert "Merge pull request #1 from Wttag/vktr"""
    This reverts commit f8cb6eb0a3294cbdc2b77591ade52d00b073f0cf.

--- Character/Player.gd
+++ Character/Player.gd
@@ -9,12 +9,6 @@ const JUMP_VELOCITY = -1000.0
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 @onready var animated_sprite = $AnimatedSprite2D
 
-func _input(event):
-	if event is InputEventKey:
-		if event.keycode == KEY_SPACE:
-			if event.is_pressed():
-				get_node("/root/Tutorial").set_toggle()
-
 func _physics_process(delta):
 	# Add the gravity.
 	if not is_on_floor():
@@ -30,7 +24,7 @@ func _physics_process(delta):
 			animated_sprite.animation = "Run"
 
 	# Handle Jump.
-	if Input.is_action_just_pressed("ui_up") and is_on_floor():
+	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 	
 		
