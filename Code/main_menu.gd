7fa00b4d9c938fe569e9fcdee1f4f52565f158ae
Wttag <fancsa100@gmail.com>
2023-04-29 17:40:01 +0200

    Menu conrolls 0.0

---
+++ Code/main_menu.gd
@@ -0,0 +1,21 @@
+extends Control
+
+func _on_start_pressed():
+	get_tree().change_scene_to_file("res://Level/tutorial.tscn")
+	
+
+
+func _on_book_of_yokai_pressed():
+	pass # Replace with function body.
+
+
+func _on_map_of_yoni_pressed():
+	pass # Replace with function body.
+
+
+func _on_options_pressed():
+	get_tree().change_scene_to_file("res://Code/options_menu.tscn")
+
+
+func _on_quit_pressed():
+	get_tree().quit()
