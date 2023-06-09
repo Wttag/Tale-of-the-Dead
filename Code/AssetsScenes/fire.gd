bfeb4d5179c0d85131fb6c314f2ffcb51d627182
Wttag <fancsa100@gmail.com>
2023-06-09 21:38:24 +0200

    Parallax bg update

--- Code/AssetsScenes/fire.gd
+++ Code/AssetsScenes/fire.gd
@@ -4,7 +4,7 @@ extends Area2D
 
 func fire_on_body_entered(body):
 	$AnimationPlayer.play("Bounce")
-	body.add_fire()
+	body.add_flame()
 
 
 func _on_animation_player_animation_finished(anim_name):
