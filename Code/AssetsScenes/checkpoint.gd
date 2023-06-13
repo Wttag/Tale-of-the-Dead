8736bb628048f05c69ef662fb4e725786d05caff
David Osaro <90871488+davidosaro@users.noreply.github.com>
2023-06-13 21:26:58 +0100

    fixed checkpoint animation

--- Code/AssetsScenes/checkpoint.gd
+++ Code/AssetsScenes/checkpoint.gd
@@ -1,10 +1,11 @@
 extends Area2D
 
-@onready var parent = get_parent()
+@onready var parent = get_parent().get_parent()
+@onready var anim = $AnimationPlayer
 
 func _on_body_entered(body):
 	if body.get_groups().has("player"):
-		$AnimationPlayer.play("Activate")
+		anim.play("Activate")
 		$Sound_Checkpoint.play()
 		print("entered")
 		parent.set_checkpoint_position(global_position)
