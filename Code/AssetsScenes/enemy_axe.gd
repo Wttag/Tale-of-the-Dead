f1c3a929b4a3a549658b44d6e16c26d3996c41c8
David Osaro <90871488+davidosaro@users.noreply.github.com>
2023-06-14 22:25:24 +0100

    added shift to the axe

--- Code/AssetsScenes/enemy_axe.gd
+++ Code/AssetsScenes/enemy_axe.gd
@@ -2,13 +2,19 @@ extends Area2D
 
 @export var rotating = false
 @onready var anim = $AnimationPlayer
-
+@onready var dark = $Dark
+@onready var light = $Light
 
 func _ready():
 	if rotating:
 		anim.play("swing")
 	pass
-	
+
+func set_shift(condition):
+	light.visible = !condition
+	dark.visible = condition
+	pass
+
 func _on_body_entered(body):
 	if body.get_groups().has("player"):
 		get_tree().change_scene_to_file("res://Level/tutorial.tscn")
