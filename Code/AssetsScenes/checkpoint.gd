9be6952f1520a14151e36abe0a9770863abb73e3
David Osaro <90871488+davidosaro@users.noreply.github.com>
2023-06-14 21:57:19 +0100

    finished first bugs

--- Code/AssetsScenes/checkpoint.gd
+++ Code/AssetsScenes/checkpoint.gd
@@ -2,10 +2,11 @@ extends Area2D
 
 @onready var parent = get_parent().get_parent()
 @onready var anim = $AnimationPlayer
+var entered = false
 
 func _on_body_entered(body):
-	if body.get_groups().has("player"):
+	if body.get_groups().has("player") and not entered:
 		anim.play("Activate")
 		$Sound_Checkpoint.play()
 		parent.set_checkpoint_position(global_position)
-		
+		entered = true
