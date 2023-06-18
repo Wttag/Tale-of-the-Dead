ff905914d04fc2c8d42f5e4c19b3a7fc6d719fa3
David Osaro <90871488+davidosaro@users.noreply.github.com>
2023-06-18 22:49:29 +0100

    fixed dialog font size and end text

--- Dialogue/dialogs.gd
+++ Dialogue/dialogs.gd
@@ -15,25 +15,29 @@ var dark_dialog = [
 	"Spare me with the questioning",
 	"You are now dead and locked up here for eternity, that's it.",
 	"Now move along, I have better things to do!",
+	"..."
 ]
 
 var dark_died_dialog = [
 	"Haha! You just died and the first thing you do is die again?",
 	"The dead can’t die twice, you fool.",
 	"No use trying a weak soul like you could never get out!",
+	"..."
 ]
 
 var light_dialog = [
 	"Ah, yeah you must be new here! ",
 	"Well, you are dead now I’m afraid so. Do not be scared we all are, this is the Land of the Dead after all",
 	"You are safe here but be careful this place is dangerous for such a small soul like you!",
+	"..."
 ]
 
 var light_died_dialog = [
 	"Haha! I see you have not listened.",
 	"No worries, you will be back at the gates every time you fail...",
 	"The gates are what bind the lost souls to this place, so they shall never leave.",
-	"Many tried but aloof them have failed."
+	"Many tried but aloof them have failed.",
+	"..."
 ]
 
 func _ready():
